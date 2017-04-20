9006f5122d6059f63a473e530e3f03b8e37e5ace
shynome <shynome@gmail.com>
2017-04-21 01:54:53 +0800

    小优化

--- src/watch.ts
+++ src/watch.ts
@@ -1,7 +1,7 @@
 /**监听文件变化并清除缓存 */
-export const watch = (dirname:string)=>
+export const watch = (dirname:string=process.cwd())=>
 require('chokidar')
-  .watch(process.cwd() || dirname,{
+  .watch(dirname,{
     ignored:/\.git/,
   })
   .on('change',function(path,stat){
