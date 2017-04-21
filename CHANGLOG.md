c97ac096545c0c2fb7ac9ad02726e9ad3cf32b11
shynome <shynome@gmail.com>
2017-04-21 22:15:47 +0800

    发布 1.1.4 版本

---
+++ CHANGLOG.md
@@ -0,0 +1,3 @@
+
+# 1.1.4
+only proxy function and Module.exports , don't proxy object anymore
--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,6 @@ export function ProxyFunction(func:Function,getTarget:()=>any){
     }
   })
 }
-/**鬼知道你们会在 this 上干什么事 , 比如构建实例之类的 , 等等这好像是我的问题 , 那就只代理函数和模块了 */
-export function ProxyObject(obj:Object,getTarget:()=>any){
-  return new Proxy(obj,{
-    get(obj,key){
-      let newGetTarget = ()=>getTarget()[key]
-      return ProxyFunction(newGetTarget(),newGetTarget)
-    }
-  })
-}
 export function ProxyModule(obj:Object,getTarget:()=>any){
   return new Proxy(obj,{
     get(obj,key){
@@ -32,15 +23,14 @@ function dynamicRequire(this:NodeModule,request){
   let getTarget = ()=>this.originRequire(request)
   let exports = getTarget()
   switch(true){
-    // case request[0] !== '.': //过滤掉非相对路径的模块
+    case request[0] !== '.': //don't node_modules request 
     case /\\node_modules\\/.test(this.id):
+    default:
       return exports
     case typeof exports==='function':
       return ProxyFunction(exports,getTarget)
     case typeof exports==='object' && !!exports:
       return ProxyModule(exports,getTarget)
-    default:
-      return exports
   }
 }
 module.constructor.prototype.require = dynamicRequire
