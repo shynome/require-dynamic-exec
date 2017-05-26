6e4e2b95832152cf5ca66e0d64d1fc8ecd430987
shynome <shynome@gmail.com>
2017-05-26 22:45:49 +0800

    不监听不加载的文件

--- src/watch.ts
+++ src/watch.ts
@@ -1,9 +1,25 @@
 
 import chokidar = require('chokidar')
+export const ext = 
+  new RegExp(
+    `\\\.(${Object.keys(require.extensions).map(ext=>ext.slice(1)).join('|')})$`,
+    'i'
+  )
+import { Stats } from "fs";
 /**监听文件变化并清除缓存 */
 export const watch = (basedir:any=process.cwd(),clearRequireTree=false)=>
 chokidar
-  .watch(basedir, { ignored:/\.git/, })
+  .watch( basedir, { ignored(f:string,stat:Stats){
+    switch(true){
+    case !stat:
+    case stat.isDirectory():
+      return false
+    case ext.test(f):
+      return false
+    default:
+      return true
+    }
+  }, })
   .on( 'change', clearRequireTree ? clearRequireTreeCache : clearRequireCache )
 
 export let clearRequireCache = (path:string)=>require.cache[path] && delete require.cache[path]
