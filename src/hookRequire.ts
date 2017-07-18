a63869479d96c1998ffdd7ccd6701a2cbf8313b8
shynome <shynome@gmail.com>
2017-07-18 23:40:58 +0800

    fixed : be required file not watch , now will watch the children from the parent , but the parent not watch

--- src/hookRequire.ts
+++ src/hookRequire.ts
@@ -1,22 +1,26 @@
 import { ProxyFunction,ProxyModule } from "./proxyModule";
 //save origin require
 module.constructor.prototype.originRequire = module.constructor.prototype.require
-import { watchRequireFile } from './clearCache'
+import { watchRequireFile } from './'
 //proxy module
 module.constructor.prototype.require = hookRequire
 export function hookRequire(this:NodeModule,request){
   let getTarget = ()=>this.originRequire(request)
   let exports = getTarget()
   switch(true){
-    case !(/\/|\\/.test(request))://ignore require node_module and core module
-    case /node_modules/.test(this.id)://ignore request from node_modules 
+  case !(/\/|\\/.test(request))://ignore require node_module and core module
+  case /node_modules/.test(this.id)://ignore request from node_modules 
+    return exports  
+  default:
+  this.children.map(({filename})=>filename).forEach(watchRequireFile)
+    watchRequireFile(this.id)
+    switch(true){
     default:
       return exports
     case typeof exports==='function':
-      watchRequireFile(this.id)
       return ProxyFunction(exports,getTarget)
     case typeof exports==='object' && !!exports:
-      watchRequireFile(this.id)
       return ProxyModule(exports,getTarget)
+    }
   }
 }
