73558aa629b850c164d5ffb4999d16a471458f28
shynome <shynome@gmail.com>
2017-07-18 23:39:43 +0800

    update:  exports.watch will be exec once

--- src/clearCache.ts
+++ src/clearCache.ts
@@ -1,5 +1,11 @@
 import fs = require('fs')
-export let watchRequireFile = (file:string)=>{}
+export const clearRequireCache = (path:string)=>require.cache[path] && delete require.cache[path]
+export const clearRequireTreeCache = (path:string)=>{
+  if(!require.cache[path])return
+  let parent:NodeModule = require.cache[path].parent
+  delete require.cache[path]
+  return parent && clearRequireTreeCache(parent.id)
+}
 export const watchers = {}
 export const watchFile = (clearCache:clearCache)=>(path:string)=>{
   if(watchers[path]){ return }
@@ -17,18 +23,5 @@ export const watchFile = (clearCache:clearCache)=>(path:string)=>{
   })
 }
 export type clearCache = (path:string)=>boolean
-/**
- * start watch file which is required  , for clear require cahce when it has changed 
- * @param ___dirname it will not be used 
- * @param clearTree true from strong clear method
- */
-export const watch = (__dirname:string=undefined,clearTree:boolean=false)=>{
-  watchRequireFile = watchFile(clearTree ? clearRequireTreeCache : clearRequireCache)
-}
-export const clearRequireCache = (path:string)=>require.cache[path] && delete require.cache[path]
-export const clearRequireTreeCache = (path:string)=>{
-  if(!require.cache[path])return
-  let parent:NodeModule = require.cache[path].parent
-  delete require.cache[path]
-  return parent && clearRequireTreeCache(parent.id)
-}
+import { watch_mode as clearTree } from "./";
+export let watchRequireFile = watchFile(clearTree ? clearRequireTreeCache : clearRequireCache)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 import './global'
-export { watch } from './clearCache'
+export let watch_mode = false
+import { clearCache } from "./clearCache";
+export { watchers } from "./clearCache";
+export let watchRequireFile:(path:string)=>void = (path)=>false
+/**
+ * start watch file which is required  , for clear require cahce when it has changed 
+ * @param ___dirname it will not be used 
+ * @param clearTree true from strong clear mode
+ */
+export let watch = (__dirname,mode=watch_mode)=>{
+  watch_mode = mode
+  watchRequireFile = require('./clearCache').watchRequireFile
+}
 export { hookRequire } from './hookRequire'
