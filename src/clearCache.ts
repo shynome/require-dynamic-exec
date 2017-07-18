97b8558d022316803287d86d2b2b4da527663308
shynome <shynome@gmail.com>
2017-07-18 20:24:10 +0800

    fixed: should have default value

--- src/clearCache.ts
+++ src/clearCache.ts
@@ -15,7 +15,12 @@ export const watchFile = (clearCache:clearCache)=>(path:string)=>{
   })
 }
 export type clearCache = (path:string)=>boolean
-export const watch = (__dirname:string,clearTree:boolean)=>{
+/**
+ * start watch require file , for clear require cahce when it has changed 
+ * @param ___dirname it will not be used 
+ * @param clearTree true from strong clear method
+ */
+export const watch = (__dirname:string=undefined,clearTree:boolean=false)=>{
   watchRequireFile = watchFile(clearTree ? clearRequireTreeCache : clearRequireCache)
 }
 export const clearRequireCache = (path:string)=>require.cache[path] && delete require.cache[path]
