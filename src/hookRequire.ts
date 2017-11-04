07895d29ef50f4e12becdac784b866b30dce4ad2
shynome <shynome@gmail.com>
2017-11-04 15:43:23 +0800

    添加强制代理参数

--- src/hookRequire.ts
+++ src/hookRequire.ts
@@ -4,23 +4,24 @@ module.constructor.prototype.originRequire = module.constructor.prototype.requir
 import { watchRequireFile } from './'
 //proxy module
 module.constructor.prototype.require = hookRequire
-export function hookRequire(this:NodeModule,request){
+export function hookRequire(this:NodeModule,request,forced_proxy=false){
   let getTarget = ()=>this.originRequire(request)
   let exports = getTarget()
   switch(true){
-  case !(/\/|\\/.test(request))://ignore require node_module and core module
-  case /node_modules/.test(this.id)://ignore request from node_modules 
-    return exports  
-  default:
-    this.children.map(({filename})=>filename).forEach(watchRequireFile)
-    watchRequireFile(this.id)
-    switch(true){
-      default:
-        return exports
-      case typeof exports==='function':
-        return ProxyFunction(exports,getTarget)
-      case typeof exports==='object' && !!exports:
-        return ProxyModule(exports,getTarget)
-    }
+    case forced_proxy:
+    default:
+      this.children.map(({filename})=>filename).forEach(watchRequireFile)
+      watchRequireFile(this.id)
+      switch(true){
+        default:
+          return exports
+        case typeof exports==='function':
+          return ProxyFunction(exports,getTarget)
+        case typeof exports==='object' && !!exports:
+          return ProxyModule(exports,getTarget)
+      }
+    case !(/\/|\\/.test(request))://ignore require node_module and core module
+    case /node_modules/.test(this.id)://ignore request from node_modules 
+      return exports  
   }
 }
