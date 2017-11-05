0767e85957bb3b504b18972a9bb5656216d43033
shynome <shynome@gmail.com>
2017-11-05 17:37:19 +0800

    replace `request` with `supertest`

--- test/express/index.js
+++ test/express/index.js
@@ -1,22 +1,31 @@
 require('../../').watch(__dirname)
-/**
- * @type {number}
- */
-exports.port = null
 const express = require('express')
 const app = exports.app = express()
 const router = express.Router()
 router.use(require('./router').router)
 app.use(router)
-exports.createServer = new Promise((resolve,reject)=>{
-  app.listen(function(err){
-    if(err){
-      reject(err)
-      return
-    }
-    exports.port = this.address().port
-    console.log('port:'+exports.port)
-    resolve(exports.port)
+//test.js
+const assert = require('assert')
+const util = require('util')
+const request = require('supertest')
+let now = '1'
+const fs = require('fs')
+const dynamicFile = require.resolve('./router.js')
+const toggle = async()=>{
+  now = now === '1' ? '2' : '1'
+  fs.writeFileSync(dynamicFile,`exports.router=require('./router${now}').router`)
+  await new Promise(rl=>setTimeout(rl,200))
+  return now
+}
+const server = require('.').app
+describe('express router dynamic toggle',()=>{
+  for (var index = 0; index < 4; index++) {
+    it(`router ${index}`,async()=>{
+      let now = await toggle()
+      let res = await request(server).get('/').expect(now)
+    })
+  }
+  it('clear',()=>{
+    require('../../').unwatch()
   })
 })
-require('./test')
